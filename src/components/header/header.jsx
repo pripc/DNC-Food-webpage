5cf54b9c13ce6234187b962150e1550c21ee971f
pripc <117996744+pripc@users.noreply.github.com>
2023-03-13 16:44:08 -0300

    Estilizando componente Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
+import carrinho from '../../assets/cart.png'
 import './index.scss'
 
 const header = () => {
   return (
     <header className='header'>
-        <div className='header__logo'>
+        <div className='header__leftSide'>
             <img src={logo} />
-            <h2>Food</h2>
-        </div>
-        <div className='header__list'>
-            <lu>
+            <h2>Food</h2>        
+            <ul className='header__list'>
                 <li>Variedades</li>
                 <li>Pizzas</li>
                 <li>Lanches</li>
                 <li>Doces</li>
                 <li>Promoções</li>
-            </lu>
+            </ul>
         </div>
-      
-
+        <div className='header__cart'>
+            <h4>Minhas compras</h4>
+            <img width='48px' src={carrinho}/>
+        </div>
     </header>
   )
 }
